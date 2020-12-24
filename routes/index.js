d9f1e1decba5db8d4b99d002f2921208c888ae21
Ramazan KAYA <kaya@Ramazans-MacBook-Pro.local>
2020-12-24 13:31:19 +0300

    express import

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/your-app-name');
+
+const mongoConnect = async () =>{
+  let result = await mongoose.connect('mongodb://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  }) 
+  return result;
+}
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  let res = await mongoConnect
+  res.send(res);
 });
 
 module.exports = router;
