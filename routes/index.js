48fe2bca3be4d99deb6d897c5774d33f68aa5f47
Ramazan KAYA <kaya@Ramazans-MacBook-Pro.local>
2020-12-24 21:00:26 +0300

    express import

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,88 @@
 var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema
 const mongoURI = 'mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true';
 
-// notice the mongoose.createConnection instead of mongoose.connect
-mongoose.connect(mongoURI, { 
-    useNewUrlParser: true, 
-    useCreateIndex: true, 
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true
-}); 
-const Records = mongoose.model('records', { }); 
-const getRecordsByFilter = async (startedDate,endingDate,minLimit,maxLimit) =>{
- return new Promise((res,rej)=>{
-    Records.aggregate([
-    {
-        $match: {
-            createdAt: {
-                $gte: new Date(startedDate), $lt: new Date(endingDate)
-            }
-        }
-    }, 
-    {
-        $project: {
-            key:"$key",
-            createdAt:"$createdAt",
-            _id: "$key", 
-            total: {
-                $sum: "$counts"
-            }
-        }
-    } ,
-    {
-        $match: {
-            total: {
-                $gte: minLimit, $lt: maxLimit
-            }
-        }
-    }
-  ], function (err, docs) { 
-    if (err){ 
-        console.log(err); 
-    } 
-    else{  
-        res(docs)
-    } 
-  }); 
-
- })
-}
- 
-/* GET home page. */
-router.get('/:sDate/:eDate/:min/:max', async function (req, res, next) { 
-let {sDate, eDate, min,max} = req.params
-console.log(req.params)
-let response = await getRecordsByFilter(sDate,eDate,parseInt(min),parseInt(max))
- 
-  res.send(JSON.stringify(response));
 });
+
+const Records = mongoose.model('records', {});
+
+
+const getRecordsByFilter = async (startDate, endDate, minCount, maxCount) =>
+    new Promise((res, rej) => {
+        Records.aggregate([
+            {
+                $match: {
+                    createdAt: {
+                        $gte: new Date(startDate), $lt: new Date(endDate)
+                    }
+                }
+            },
+            {
+                $project: {
+                    key: "$key",
+                    createdAt: "$createdAt",
+                    _id: "$key",
+                    totalCount: {
+                        $sum: "$counts"
+                    }
+                }
+            },
+            {
+                $match: {
+                    totalCount: {
+                        $gte: minCount, $lt: maxCount
+                    }
+                }
+            }
+        ], function (err, docs) {
+            if (err) {
+                rej(err);
+            }
+            else {
+                res(docs);
+            }
+        });
+
+    })
+
+const checkParams = (req, res, next) => {
+    let { startDate, endDate, minCount, maxCount } = req.body;
+    let regexDate = new RegExp('^[0-9]{4}-[0-9]{2}-[0-9]{2}$');
+    let regexNumber = new RegExp('^[0-9]*$');
+    if (regexDate.test(startDate) && regexDate.test(endDate)) {
+        if (regexNumber.test(minCount) && regexNumber.test(maxCount) && maxCount > minCount) {
+            next();
+        }
+        else {
+            res.status(300).send({ code: 1, msg: 'Something went wrong. Check min and max numbers !!' });
+        }
+    } else {
+        res.status(300).send({ code: 1, msg: 'Something went wrong. Check Date Format It should be (YYYY-MM-DD) !!' });
+    }
+
+} 
+
+router.get('/', function (req, res, next) {
+    
+    res.send({message:req.url})
+});
+
+router.post('/', checkParams, async function (req, res, next) {
+
+    let { startDate, endDate, minCount, maxCount } = req.body;
+    getRecordsByFilter(startDate, endDate, parseInt(minCount), parseInt(maxCount))
+        .then(result => {
+            res.status(200).send({ code: 0, msg: 'Success', records: result });
+        })
+        .catch(err => {
+            res.status(300).send({ code: 1, msg: 'Something went wrong. Check your params !!' });
+        })
+
+})
 
 module.exports = router;
