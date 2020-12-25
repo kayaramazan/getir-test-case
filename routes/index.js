bb6e679790b73d9bb3514a8e8de5f9b7509f9013
Ramazan KAYA <kaya@Ramazans-MacBook-Pro.local>
2020-12-25 14:57:55 +0300

    group by filtered

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const controller = require('../controller/controller')
 router.get('/', function (req, res, next) {
     res.status(200).send({ 
         code: 4,
-        msg: `Please using post method : ${req.protocol}://${req.get('host')}${req.originalUrl}`,
+        msg: `Please use the post method : ${req.protocol}://${req.get('host')}${req.originalUrl}`,
         gitRepository:'https://github.com/kayaramazan/getir-test-case'
     })
 });
