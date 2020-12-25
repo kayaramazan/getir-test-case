26309ec145a1e55f129d4149dfa3410b93d1e6f7
Ramazan Kaya <kaya-ramazan@outlook.com>
2020-12-25 13:26:29 +0300

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -57,30 +57,35 @@ const getRecordsByFilter = async (startDate, endDate, minCount, maxCount) =>
     })
 
 
-// Check if all params is valid
-const checkParams = (req, res, next) => {
-     
-    let { startDate, endDate, minCount, maxCount } = req.body;
-    
+// Check if Date params is valid
+const checkDateParams = (req, res, next) => {
+    let { startDate, endDate } = req.body;
+
     // Date format regex
     let regexDate = new RegExp('^[0-9]{4}-[0-9]{2}-[0-9]{2}$');
-
-    let regexNumber = new RegExp('^[0-9]*$');
-    if (regexDate.test(startDate) && regexDate.test(endDate)) {
-        if (regexNumber.test(minCount) && regexNumber.test(maxCount) && maxCount > minCount) {
-            next();
-        }
-        else {
-            res.status(300).send({ code: 2, msg: 'Something went wrong. Check min and max numbers !!' });
-        }
-    } else {
+    if (!(regexDate.test(startDate) && regexDate.test(endDate)))
         res.status(300).send({ code: 3, msg: 'Something went wrong. Check Date Format It should be (YYYY-MM-DD) !!' });
-    }
+    else
+        next()
 
+
+}
+// Check if Count params is valid 
+const checkCountParams = (req, res, next) => {
+
+    let {minCount, maxCount } = req.body; 
+    
+    // Number format regex
+    let regexNumber = new RegExp('^[0-9]*$');
+
+    if (!(regexNumber.test(minCount) && regexNumber.test(maxCount) && maxCount > minCount))
+        res.status(300).send({ code: 2, msg: 'Something went wrong. Check min and max numbers !!' });
+    else
+        next()
 }
 
 const getRecords = async (req, res, next) => {
-    
+
     let { startDate, endDate, minCount, maxCount } = req.body;
 
     getRecordsByFilter(startDate, endDate, parseInt(minCount), parseInt(maxCount))
@@ -93,6 +98,7 @@ const getRecords = async (req, res, next) => {
 
 }
 
-module.exports.checkParams = checkParams;
+module.exports.checkDateParams = checkDateParams;
+module.exports.checkCountParams = checkCountParams;
 module.exports.getRecords = getRecords;
 module.exports.getRecordsByFilter = getRecordsByFilter;
