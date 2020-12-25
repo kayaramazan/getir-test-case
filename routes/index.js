4b5d63f3745af4c424823e33c96ce0756a66159a
Ramazan Kaya <kaya-ramazan@outlook.com>
2020-12-25 13:29:34 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const controller = require('../controller/controller')
 
 router.get('/', function (req, res, next) {
-    res.status(200).send({ code: 4, msg: `Please using post method : ${req.protocol}://${req.get('host')}${req.originalUrl}` })
+    res.status(200).send({ 
+        code: 4,
+        msg: `Please using post method : ${req.protocol}://${req.get('host')}${req.originalUrl}`,
+        gitRepository:'https://github.com/kayaramazan/getir-test-case'
+    })
 });
 
 router.post('/', controller.checkCountParams,controller.checkDateParams, controller.getRecords)
